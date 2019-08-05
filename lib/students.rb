c0da2d3e0b919aa56d3c67885b43fb8285076c3f
Tracy Reuther <tracyreuther@gmail.com>
2019-08-05 22:21:27 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-    "SELECT MIN "
+    "SELECT MIN(gpa)"
 end
 
 def lowest_student_gpa
